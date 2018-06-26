fd7bee6875eb36d0e1a4297ca40c65a00e373528
İlker Kanber <ilkerkanber1903@gmail.com>
2018-06-26 16:08:04 +0300

    Animations method

--- app/src/main/java/com/ngdroidapp/Animations.java
+++ app/src/main/java/com/ngdroidapp/Animations.java
@@ -1,26 +1,51 @@
 package com.ngdroidapp;
 
-public class Animations
-{   private Character character;
+import istanbul.gamelab.ngdroid.util.Utils;
+
+public class Animations {
+    private Character character = new Character();
     //Nesnenin X ve Y Konumu
-    private int objectX,objectY;
+    private int objectX, objectY;
     //Hedefin X ve Y Konumu
-    private int targetX,targetY;
+    private int targetX, targetY;
     //Karakterin X ve Y Konumu
-    private int characterX,characterY;
+    private int characterX, characterY, characterWidth, characterHeight;
 
     public Animations(Character character) {
         //Character Sınıfından karakterin X ve Y'sinin çekilmesi
-    this.character = character;
-    Character c=new Character();
-    characterX=c.getNobjectdstx();
-    characterY=c.getNobjectdsty();
+        this.character = character;
 
+        characterX = character.getNobjectdstx();
+        characterY = character.getNobjectdstx();
+        characterWidth = character.getNobjectdstw();
+        characterHeight = character.getNobjectdsth();
+        objectX = characterX;
+        objectY = characterY;
+    }
+
+    public boolean Collision() {/*
+    //Objelerin Karşılaştırılması
+if(Utils.checkCollision(character.getNobjectdestination(),character.getNobjectdestination())
+    {
+        return true;
+    }
+    else{
+    return false;
 }
+*/
+return false;
+    }
 
-public void shout()
-{
 
+    public void ShoutAnımation() {/*
+    if(!Collision())
+    { objectX++;
+    character.setNobjectdstx(objectX);}
+    else
+    {
+    }
 }
+*/
 
+    }
 }
