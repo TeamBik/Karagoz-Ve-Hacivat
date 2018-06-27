773b6a908ff665439715e73ef835ec69153311ef
murakadir12 <alikadirkurt@hotmail.com>
2018-06-27 14:22:30 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/ngdroidapp/GameCanvas.java

--- app/src/main/java/com/ngdroidapp/GameCanvas.java
+++ app/src/main/java/com/ngdroidapp/GameCanvas.java
@@ -15,12 +15,14 @@ public class GameCanvas extends BaseCanvas {
 
     Character karagoz, hacivat;
     Animations animKaragoz, animHacivat;
-    Nobject arkaplan, obje1;
+    Nobject arkaplan, obje1, backbutton;
+    private int touchdownx, touchdowny;
 
     public void setup() {
         karagoz = new Character();
         arkaplan = new Nobject();
         obje1 = new Nobject();
+        backbutton = new Nobject();
         animKaragoz = new Animations(karagoz,hacivat);
         animHacivat = new Animations(hacivat,karagoz);
         setupHacivat();
@@ -28,6 +30,7 @@ public class GameCanvas extends BaseCanvas {
         arkaplan.setNobject(Utils.loadImage(root, "arkaplan.png"));
         karagoz.setNobject(Utils.loadImage(root,"karagoz.png"));
         hacivat.setNobject(Utils.loadImage(root,"hacivat.png"));
+        backbutton.setNobject(Utils.loadImage(root,"backbutton.png"));
 
     }
     public void setupHacivat(){
@@ -69,6 +72,10 @@ public class GameCanvas extends BaseCanvas {
         obje1.setNobjectsource(0,0,757,720);
         obje1.setNobjectdestination(hacivat.getNobjectdstx() + 75,hacivat.getNobjectdsty() - obje1.getNobjectdsth(),15,30);
         canvas.drawBitmap(obje1.getNobject(),obje1.getNobjectsource(),obje1.getNobjectdestination(),null);
+
+        backbutton.setNobjectsource(0,0,256,256);
+        backbutton.setNobjectdestination(getWidth()-backbutton.getNobjectdstw(),0,128,128);
+        canvas.drawBitmap(backbutton.getNobject(), backbutton.getNobjectsource(), backbutton.getNobjectdestination(), null);
     }
     public void AiPlayerModeDefence(Character character){
             if(animHacivat.AIDefenceCollision()){
@@ -91,6 +98,7 @@ public class GameCanvas extends BaseCanvas {
     }
 
     public boolean backPressed() {
+        System.exit(0);
         return true;
     }
 
@@ -107,6 +115,13 @@ public class GameCanvas extends BaseCanvas {
     }
 
     public void touchDown(int x, int y, int id) {
+        touchdownx = x;
+        touchdowny = y;
+        if (x >= backbutton.getNobjectdstx() && x <= backbutton.getNobjectdstx() + backbutton.getNobjectdstw() && y >= backbutton.getNobjectdsty() && y <= backbutton.getNobjectdsty() + backbutton.getNobjectdsth()) {
+
+            MenuCanvas mc1= new MenuCanvas(root);
+            root.canvasManager.setCurrentCanvas(mc1);
+        }
     }
 
     public void touchMove(int x, int y, int id) {
